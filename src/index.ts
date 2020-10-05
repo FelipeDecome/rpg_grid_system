7f2fa299c90d2ad3f0d034ee2913151316c0ef18
Felipe Decome <felipe.decome@hotmail.com>
2020-10-05 12:17:14 -0300

    starting from scratch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { drawGrid, context } from "./factories/canvas";
+const QSA = document.querySelectorAll.bind(document);
 
-drawGrid();
+const root = QSA("#root")[0];
+
+const createCanvas = function () {
+  this.width = 600;
+  this.height = 600;
+
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  root.append(canvas);
+
+  canvas.width = this.width;
+  canvas.height = this.height;
+  canvas.style.background = "#fff";
+
+  return { context };
+};
+
+const canvas = createCanvas();
